a03cef41daf128073b4db681174e64de9a4f1f2d
Danilo Santo <d4n1l0.id.apple@gmail.com>
2025-06-22 12:33:48 +0200

    Update HealthController.java

--- back-end/crud/src/main/java/it/uniba/crud/controller/HealthController.java
+++ back-end/crud/src/main/java/it/uniba/crud/controller/HealthController.java
@@ -2,7 +2,9 @@ package it.uniba.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.jdbc.core.JdbcTemplate;
 import java.time.LocalDateTime;
@@ -10,6 +12,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+@RestController 						//Dice a Spring che questa classe gestisce richieste REST (risponde con JSON*/
+@RequestMapping("/api/v1/keep-alive") 	//Tutte le richieste/URL che iniziano con /api/v1/keep-alive devono essere gestite da questo controller.*/
+@CrossOrigin(maxAge = 3360)				//Abilita le richieste CORS (cioè da altri domini, tipo dal tuo frontend Angular)*/
 
 public class HealthController {
 	
